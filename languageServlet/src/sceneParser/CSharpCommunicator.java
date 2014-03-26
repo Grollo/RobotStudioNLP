13c8d6e64bc1ec6ceace9a00508aec9c01a485dc
Grollo <marcus.sebastian.kristensson@gmail.com>
2014-03-26 12:09:20 +0100

    Made a skeleton

---
+++ languageServlet/src/sceneParser/CSharpCommunicator.java
@@ -0,0 +1,20 @@
+package sceneParser;
+
+public class CSharpCommunicator {
+	
+	public CSharpCommunicator(){
+		
+	}
+	
+	public void create(int id, String model){
+		
+	}
+	
+	public void remove(int id){
+		
+	}
+	
+	public void modify(int id, String attribute, String value){
+		
+	}
+}
