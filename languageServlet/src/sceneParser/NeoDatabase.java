aad5c23592ea8dc0c734ed29b2c8405112264e05
Hampus <hampus.mauritzon@gmail.com>
2014-03-26 13:02:19 +0100

    Added comments to methods in NeoDatabase.

--- languageServlet/src/sceneParser/NeoDatabase.java
+++ languageServlet/src/sceneParser/NeoDatabase.java
@@ -7,26 +7,43 @@ public class NeoDatabase {
 
 	GraphDatabaseService db;
 	
-	public boolean addItemType(){
-		return false;
-	}
+//	/**Adds a new itemType to the database.
+//	 * @return <code>true</code> if the itemType was added to the database. Else returns <code>false</code>.*/
+//	public boolean addItemType(){
+//		//TODO implement
+//		return false;
+//	}
 	
+	/**Creates a new item in the database.
+	 * @param itemType - the Type of item to be created.
+	 * @return <code>true</code> if a item was created. Else returns <code>false</code>.*/
 	public boolean createItem(){
+		//TODO implement
 		return false;
 	}
 	
-	public boolean removeItem(){
+	/**Removes an existing item from the database.
+	 * @param item - the item to be removed.
+	 * @return <code>true</code> if the item was removed. Else returns <code>false</code>.*/
+	public boolean removeItem(Item item){
+		//TODO implement
 		return false;
 	}
 	
-	public boolean modifyItem(){
+	/**Sets the specified attribute of the specified item to the specified value,
+	 * @param item - the item to be modified.
+	 * @param attribute - the attribute to be modified.
+	 * @param value - the value to be assigned.
+	 * @return <code>true</code> if the modification was successful. Else returns <code>false</code>.*/
+	public boolean modifyItem(Item item){
+		//TODO implement
 		return false;
 	}
 	
 	/**Connects to the specified Neo4j-server.
 	 * @param urlPath - The URL of the Neo4j-server.
 	 * 
-	 * @return <code>true</code> if connection was established.*/
+	 * @return <code>true</code> if connection was established. Else returns <code>false</code>.*/
 	public boolean connect(String urlPath){
 		//urlPath = "localhost:7474"
 		db = new RestGraphDatabase(urlPath);
@@ -34,8 +51,13 @@ public class NeoDatabase {
 		return true;
 	}
 	
+	
+	/**Closes the connection to the Neo4j-database,*/
 	public void disconnect(){
-		db.shutdown();
+		if(db != null){			
+			db.shutdown();
+			db = null;
+		}
 	}
 	
 	private static void registerShutdownHook(final GraphDatabaseService db) {
