8563c46b4199644094271ec364078f0ba004d9ec
Hampus <hampus.mauritzon@gmail.com>
2014-03-26 12:38:43 +0100

    Added Neo4j libraries.

--- languageServlet/src/sceneParser/NeoDatabase.java
+++ languageServlet/src/sceneParser/NeoDatabase.java
@@ -7,12 +7,31 @@ public class NeoDatabase {
 
 	GraphDatabaseService db;
 	
-	public boolean connect(String Path){
-		//Path = "localhost:7474"
-		db = new RestGraphDatabase(Path);
-		registerShutdownHook(db);
-		
+	public boolean addItemType(){
 		return false;
+	}
+	
+	public boolean createItem(){
+		return false;
+	}
+	
+	public boolean removeItem(){
+		return false;
+	}
+	
+	public boolean modifyItem(){
+		return false;
+	}
+	
+	/**Connects to the specified Neo4j-server.
+	 * @param urlPath - The URL of the Neo4j-server.
+	 * 
+	 * @return <code>true</code> if connection was established.*/
+	public boolean connect(String urlPath){
+		//urlPath = "localhost:7474"
+		db = new RestGraphDatabase(urlPath);
+		registerShutdownHook(db);
+		return true;
 	}
 	
 	public void disconnect(){
