431109f00cc5ff0882a6e2e50664a8a6c80e2950
Grollo <marcus.sebastian.kristensson@gmail.com>
2014-03-31 11:09:52 +0200

    added getMModel

--- languageServlet/src/sceneParser/Main.java
+++ languageServlet/src/sceneParser/Main.java
@@ -5,10 +5,12 @@ import se.lth.cs.semparser.corpus.Sentence;
 
 public class Main {
 	private final static Command grammarError = Command.notify("I don't understand your grammar, can you please rephrase that?");
+	private final static String noSuchObject = "Couldn't find an object named ";
 	private int activeAgentId = -1; // id of last referred to object, -1 if no such thing
-	
+
 	public static ArrayList<Command> interpret(Sentence parsedSentence) {
-		ArrayList<Command> commands = new ArrayList<Command>();if(incorrectGrammar(parsedSentence)){
+		ArrayList<Command> commands = new ArrayList<Command>();
+		if (incorrectGrammar(parsedSentence)) {
 			commands.add(grammarError);
 		} else {
 			// TODO
@@ -16,34 +18,43 @@ public class Main {
 		return commands;
 	}
 
-	private static boolean incorrectGrammar(Sentence parsedSentence) { 
-		return false;
+	private static boolean incorrectGrammar(Sentence parsedSentence) {
+		return false; // TODO
 	}
-	
+
 	/**
 	 * @param noun
 	 * @return list of ids of items the noun could be referring to
 	 */
-	private static int[] possibleItems(String noun){ 
-		return new int[] {}; //TODO
+	private static int[] possibleItems(String noun) {
+		return new int[] {}; // TODO
 	}
-	
+
 	/**
 	 * @param name
 	 * @return ids of all items that have the name
 	 */
-	private static int[] namedItemsWithName(String name){
-		return new int[] {}; //TODO
+	private static int[] namedItemsWithName(String name) {
+		return new int[] {}; // TODO
 	}
-	
+
 	/**
 	 * @param ids
 	 * @param attribute
 	 * @param value
 	 * @return sublist of items that fit the property
 	 */
-	private static int[] filterByProperty(int[] ids, String attribute, String value){
-		return new int[] {}; //TODO
+	private static int[] filterByProperty(int[] ids, String attribute, String value) {
+		return new int[] {}; // TODO
 	}
 	
+	/**
+	 * 
+	 * @param name
+	 * @return model name
+	 */
+	private static String getModel(String name){
+		return null; //TODO
+	}
+
 }
