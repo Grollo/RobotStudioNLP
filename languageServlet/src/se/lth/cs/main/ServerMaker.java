00c439e31ce27396768bfbf43450762e2f297ca5
Grollo <marcus.sebastian.kristensson@gmail.com>
2014-04-07 13:58:53 +0200

    added servermaker

---
+++ languageServlet/src/se/lth/cs/main/ServerMaker.java
@@ -0,0 +1,21 @@
+package se.lth.cs.main;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import neo4j.NeoDatabase;
+
+public class ServerMaker implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		NeoDatabase.start("localhost:7474");
+		System.out.println("Started server correctly! :)");
+	}
+
+}
